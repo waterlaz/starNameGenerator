8f00c7490a57ed53379f8209e0cc436ce80aa67f
waterlaz <waterlaz@gmail.com>
2022-08-26 16:48:31 +0300

    Something works!!

---
+++ generator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* prefix[] = {
+    #include "prefixes.h"
+};
+
+const char* trigrams[] = {
+    #include "trigrams.h"
+};
+
+void generate(char* s, int maxLength){
+    if(maxLength<3){
+        s[0] = '\0';
+        return;
+    }
+    int nPrefix = sizeof(prefix)/sizeof(char*);
+    int nTrigrams = sizeof(trigrams)/sizeof(char*);
+    int p = rand()%nPrefix;
+    s[0] = prefix[p][0];
+    s[1] = prefix[p][1];
+    s[2] = prefix[p][2];
+    int i=3;
+    while(i<maxLength-1){
+        int t=0;
+        while(t<nTrigrams){
+            if(   s[i-3] == trigrams[t][0]
+               && s[i-2] == trigrams[t][1]
+               && s[i-1] == trigrams[t][2] ){
+               break;
+            }
+            t++;
+        }
+        /* failed to find a trigram? break in two words */
+        if(t==nTrigrams){
+            s[i] = ' ';
+            generate(s+i+1, maxLength-i-1);
+            return;
+        }
+        const char* letters = trigrams[t] + 3;
+        int n = strlen(letters);
+        s[i] = letters[rand()%n];
+        i++;
+    }
+    s[i] = '\0';
+}
+
+int main(){
+    srand(time(NULL));
+    char s[100];
+    while(1){
+        generate(s, 20);
+        printf("%s\n", s);
+    }
+}
